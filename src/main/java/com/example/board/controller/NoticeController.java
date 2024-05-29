a142ebc289620f399a0daf21039a4aa09baedee3
myo22 <dlalsgud12@naver.com>
2024-05-29 16:15:10 +0900

    이미지 수정

---
+++ src/main/java/com/example/board/controller/NoticeController.java
@@ -0,0 +1,11 @@
+package com.example.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class NoticeController {
+
+
+}
---
+++ src/main/java/com/example/board/service/NoticeService.java
@@ -0,0 +1,11 @@
+package com.example.board.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class NoticeService {
+
+
+}
