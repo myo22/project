8d357459aaef5b81b3257d98ca620d9405297302
myo22 <dlalsgud12@naver.com>
2024-07-07 14:29:41 +0900

    Querydsl로 검색 조건과 목록 처리

--- src/main/java/com/example/board/Repository/search/BoardSearchImpl.java
+++ src/main/java/com/example/board/Repository/search/BoardSearchImpl.java
@@ -2,6 +2,7 @@ package com.example.board.Repository.search;
 
 import com.example.board.domain.Board;
 import com.example.board.domain.QBoard;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,11 +19,23 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
     @Override
     public Page<Board> search(Pageable pageable) {
 
-        QBoard board = QBoard.board; // Q도메인 객체
+        QBoard board = QBoard.board;
 
-        JPQLQuery<Board> query = from(board); // select.. from board
+        JPQLQuery<Board> query = from(board);
 
-        query.where(board.title.contains("1")); // where title like ...
+        BooleanBuilder booleanBuilder = new BooleanBuilder(); // (
+
+        booleanBuilder.or(board.title.contains("11")); // title like
+
+        booleanBuilder.or(board.content.contains("11")); // content like
+
+        query.where(booleanBuilder);
+        query.where(board.boardId.gt(0L));
+
+//        query.where(board.title.contains("예시")); // where title like ...
+//
+//        //paging
+//        this.getQuerydsl().applyPagination(pageable, query);
 
         List<Board> list = query.fetch(); // JQPLQuery 실행
 
