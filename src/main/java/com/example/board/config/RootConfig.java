e00302c4d3559addc490d7d3929ac11b074286e8
myo22 <dlalsgud12@naver.com>
2024-07-08 15:32:07 +0900

    ModelMapper 설정

--- src/main/java/com/example/board/config/RootConfig.java
+++ src/main/java/com/example/board/config/RootConfig.java
@@ -12,9 +12,9 @@ public class RootConfig {
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration()
-                .setFieldMatchingEnabled(true)
-                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
-                .setMatchingStrategy(MatchingStrategies.LOOSE);
+                .setFieldMatchingEnabled(true) //필드 매칭 활성화
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE) // 필즈 접근 수준 설정
+                .setMatchingStrategy(MatchingStrategies.LOOSE); // 매칭 전략 설정, 느슨한 매칭으로 이름이 유사해도 매핑할 수 있게 함.
 
         return modelMapper;
 
