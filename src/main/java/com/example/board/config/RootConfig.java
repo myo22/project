78404a6320d0ab68ec2edc87f6e5db33e791b3d7
myo22 <dlalsgud12@naver.com>
2024-07-08 15:27:48 +0900

    ModelMapper 설정

---
+++ src/main/java/com/example/board/config/RootConfig.java
@@ -0,0 +1,23 @@
+package com.example.board.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 스프링의 설정 클래스 명시
+public class RootConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+
+        return modelMapper;
+
+    }
+
+}
