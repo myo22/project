d9bbe54bc365cfcae4f4dada9d422d54c855a457
myo22 <dlalsgud12@naver.com>
2024-07-09 16:08:45 +0900

    페이징 서비스, 검색 기능

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
@@ -22,6 +23,7 @@ import java.util.List;
 // HTTP요청을 받아서 응답을 받는 컴포넌트, 스프링 부트가 자동으로 Bean으로 생성한다.
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/board")
 public class BoardController {
 
     private final BoardServiceImpl boardService;
@@ -32,7 +34,7 @@ public class BoardController {
     // http://localhost:8080/ -----> "list"라는 이름의 템플릿을 사용(forward)하여 화면에 출력.
     // list를 리턴한다는 것은 classpath:/templates/list.html을 사용한다는 뜻이다. classpath:/경로나  .html(확장자)를 바꿔주고 싶다면 prefix랑 suffix를 바꿔주면 가능하다.
     @GetMapping("/list")
-    public String list(HttpSession httpSession,
+    public void list(HttpSession httpSession,
                        Model model,
                        PageRequestDTO pageRequestDTO,
                        @RequestParam("currentCourseId") int courseId){ // HttpSession, Model은 Spring이 자동으로 넣어준다.
@@ -61,7 +63,6 @@ public class BoardController {
         model.addAttribute("responseDTO", responseDTO);
 
 
-        return "list"; // 컨트롤러의 메소드가 리턴하는 문자열은 템플릿 이름이다.
     }
 
     // /board?id=3 // 물음표 뒤에 값은 파라미터 id, 파라미터 id의 값은 3
@@ -84,7 +85,7 @@ public class BoardController {
 
         model.addAttribute("course", course);
         model.addAttribute("board", board);
-        return "board";
+        return "board"; // 컨트롤러의 메소드가 리턴하는 문자열은 템플릿 이름이다.
     }
 
     // 삭제한다. 관리자는 모든 글을 삭제할 수 있다.
