ff6caebbffa28bdca7589a9aff63d28afc228e95
myo22 <dlalsgud12@naver.com>
2024-07-18 15:44:55 +0900

    Swagger UI 설정

---
+++ src/main/java/com/example/board/config/CustomServletConfig.java
@@ -0,0 +1,9 @@
+package com.example.board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class CustomServletConfig {
+}
