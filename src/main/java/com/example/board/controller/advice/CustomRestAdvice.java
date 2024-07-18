494eb629628cb1e4bcb1861fde1a3a8ee8349105
myo22 <dlalsgud12@naver.com>
2024-07-18 16:29:12 +0900

    컨트롤러에서 예외가 발생했을때 확인하기 위하여 Advice 설정

---
+++ src/main/java/com/example/board/controller/advice/CustomRestAdvice.java
@@ -0,0 +1,37 @@
+package com.example.board.controller.advice;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+@Log4j2
+public class CustomRestAdvice {
+
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    public ResponseEntity<Map<String, String>> handleBindException(BindException e) {
+
+        log.error(e);
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        if (e.hasErrors()){
+            BindingResult bindingResult = e.getBindingResult();
+
+            bindingResult.getFieldErrors().forEach(fieldError -> {
+                errorMap.put(fieldError.getField(), fieldError.getCode());
+            });
+        }
+
+        return ResponseEntity.badRequest().body(errorMap);
+    }
+}
