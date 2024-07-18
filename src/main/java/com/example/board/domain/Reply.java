d6a2841b7844f4de6895ccf3d792c499902d068a
myo22 <dlalsgud12@naver.com>
2024-07-18 17:37:35 +0900

    인덱스 처리, exclude

--- src/main/java/com/example/board/domain/Reply.java
+++ src/main/java/com/example/board/domain/Reply.java
@@ -12,14 +12,14 @@ import javax.persistence.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString(exclude = "board")
+@ToString(exclude = "board") // 이걸 제외하지 않으면 테스트에서 에러가 발생, Board 테이블에서 쿼리를 추가로 실행해서 board객체를 toString해야하기 때문에
 public class Reply extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long rno;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY) // 필요한 순간까지 데이터베이스와 연결하지 않는 방식
     private Board board;
 
     private String replyText;
--- src/test/java/com/example/board/repository/ReplyRepositoryTests.java
+++ src/test/java/com/example/board/repository/ReplyRepositoryTests.java
@@ -3,11 +3,17 @@ package com.example.board.repository;
 import com.example.board.Repository.ReplyRepository;
 import com.example.board.domain.Board;
 import com.example.board.domain.Reply;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 @SpringBootTest
+@Log4j2
 public class ReplyRepositoryTests {
 
     @Autowired
@@ -27,5 +33,19 @@ public class ReplyRepositoryTests {
                 .build();
 
         replyRepository.save(reply);
+    }
+
+    @Test
+    public void testBoardReplies(){
+
+        Long bno = 2L;
+
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("rno").descending());
+
+        Page<Reply> result = replyRepository.listOfBoard(bno, pageable);
+
+        result.getContent().forEach(reply -> {
+            log.info(reply);
+        });
     }
 }
