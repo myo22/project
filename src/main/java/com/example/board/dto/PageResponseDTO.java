9a57eda2dcfce49dec989fdc423e80ebcd41b373
myo22 <dlalsgud12@naver.com>
2024-07-23 18:14:25 +0900

    오류

--- src/main/java/com/example/board/dto/PageResponseDTO.java
+++ src/main/java/com/example/board/dto/PageResponseDTO.java
@@ -40,17 +40,17 @@ public class PageResponseDTO<E> {
         this.dtoList = dtoList;
 
         // 화면에서의 마지막 번호
-        this.end = (int)(Math.ceil(this.page/10.0)) * 10;
+        this.end =   (int)(Math.ceil(this.page / 10.0 )) *  10;
 
         //화면에서의 시작 번호
         this.start = this.end - 9;
 
         // 데이터의 개수를 계산한 마지막 페이지 번호
-        int last = (int)(Math.ceil(total/this.size));
+        int last =  (int)(Math.ceil((total/(double)size)));
 
         this.end = end > last ? last: end;
 
-        this.prev = total > 1;
+        this.prev = this.start > 1;
 
         this.next = total > this.end * this.size;
 
