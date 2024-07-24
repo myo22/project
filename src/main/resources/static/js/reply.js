22b7c4e9849095a99b3a86dc3ba72f74ab1fde6d
myo22 <dlalsgud12@naver.com>
2024-07-24 16:30:44 +0900

    댓글 마지막 페이지 호출

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -14,5 +14,11 @@ async function getList({bno, page, size, goLast}){
 
     const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}})
 
+    if(goLast){
+        const total = result.data.total
+        const lastPage = parseInt(Math.ceil(total/size))
+        return getList({bno:bno, page:lastPage, size:size})
+    }
+
     return result.data
 }
