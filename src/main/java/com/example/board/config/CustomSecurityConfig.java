0914a797bded174e7b8cdd94abdadf2fd60586d1
myo22 <dlalsgud12@naver.com>
2024-08-08 15:01:53 +0900

    정적 자원의 처리

--- src/main/java/com/example/board/config/CustomSecurityConfig.java
+++ src/main/java/com/example/board/config/CustomSecurityConfig.java
@@ -2,9 +2,11 @@ package com.example.board.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
@@ -18,6 +20,15 @@ public class CustomSecurityConfig {
         log.info("--------configure----------");
 
         return http.build();
+    }
+
+    // css 파일, js 파일 등 정적파일들에는 굳이 시큐리티를 적용할 필요가 없다.
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+
+        log.info("------------------web Configure-----------------------");
+
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
 }
