1272a3972f4c19e2b5703991a4e8c7ae1405258b
myo22 <dlalsgud12@naver.com>
2024-08-08 16:24:51 +0900

    자동 로그인 설정

--- src/main/java/com/example/board/config/CustomSecurityConfig.java
+++ src/main/java/com/example/board/config/CustomSecurityConfig.java
@@ -1,29 +1,54 @@
 package com.example.board.config;
 
+import com.example.board.security.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Log4j2
 @Configuration
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true) // 원하는 곳에 @PreAuthorize 혹은 @PostAuthorize를 통해서 사전, 사후의 권한 체크 가능
 public class CustomSecurityConfig {
+
+    private final DataSource dataSource;
+    private final CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        // 종류중에서 가장 무난, 해시 알고리즘으로 암호화 처리
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         log.info("--------configure----------");
 
-        // 로그인 화면에서 로그인을 진행한다는 설정
-        http.formLogin();
+        // 로그인 화면에서 로그인을 진행한다는 설정, 커스텀 로그인 페이지
+        http.formLogin().loginPage("/member/login");
+
+        // CSRF 토큰 비활성화
+        http.csrf().disable();
+
+        http.rememberMe()
+                .key("12345678") // 쿠기의 값을 인코딩하기 위한 키값
+                .tokenRepository(persistentTokenRepository())
+                .userDetailsService(userDetailsService)
+                .tokenValiditySeconds(60*60*24*30);
 
         return http.build();
     }
@@ -37,10 +62,12 @@ public class CustomSecurityConfig {
         return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
+    // 쿠키의 필요한 정보를 저장하는 메소드
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        // 종류중에서 가장 무난, 해시 알고리즘으로 암호화 처리
-        return new BCryptPasswordEncoder();
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl repo = new JdbcTokenRepositoryImpl();
+        repo.setDataSource(dataSource);
+        return repo;
     }
 
 }
--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -11,6 +11,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -104,7 +105,7 @@ public class BoardController {
 
     // 삭제한다. 관리자는 모든 글을 삭제할 수 있다.
     // 수정한다.
-
+    @PreAuthorize("hasRole('USER')")
     @GetMapping("/register")
     public void registerGET(HttpSession httpSession, Model model,
                             @RequestParam("courseId") int courseId){
