78e0c534c16a91f50dd7e59efb757bad129ddd9d
myo22 <dlalsgud12@naver.com>
2024-08-08 14:59:17 +0900

    로그 레벨 조정

---
+++ src/main/java/com/example/board/config/CustomSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.board.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Log4j2
+@Configuration
+@RequiredArgsConstructor
+public class CustomSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        log.info("--------configure----------");
+
+        return http.build();
+    }
+
+}
