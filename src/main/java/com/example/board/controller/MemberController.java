f11a3ea0d64a04f8f2b7b385161e53a0446eeae4
myo22 <dlalsgud12@naver.com>
2024-08-14 16:19:56 +0900

    MSA 방식 아키텍처

--- src/main/java/com/example/board/controller/MemberController.java
+++ src/main/java/com/example/board/controller/MemberController.java
@@ -1,16 +1,21 @@
 package com.example.board.controller;
 
+import com.example.board.Repository.MemberRepository;
+import com.example.board.dto.MemberJoinDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/member")
 @Log4j2
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberRepository memberRepository;
 
     @GetMapping("/login")
     public void loginGET(String error, String logout){
@@ -20,6 +25,20 @@ public class MemberController {
         if (logout != null){
             log.info("user logout........");
         }
+    }
+
+    @GetMapping("/join")
+    public void joinGET(){
+        log.info("join get.....");
+    }
+
+    @PostMapping("/join")
+    public String joinPOST(MemberJoinDTO memberJoinDTO){
+        log.info("join post.....");
+        log.info(memberJoinDTO);
+
+        return "redirect:/";
+
     }
 
 }
---
+++ src/main/java/com/example/board/dto/MemberJoinDTO.java
@@ -0,0 +1,13 @@
+package com.example.board.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberJoinDTO {
+
+    private String mid;
+    private String mpw;
+    private String email;
+    private boolean del;
+    private boolean social;
+}
