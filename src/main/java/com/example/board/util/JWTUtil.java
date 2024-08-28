3c6c335e4dc2708c701ba7eeac989594daae4cf1
myo22 <dlalsgud12@naver.com>
2024-08-28 16:37:28 +0900

    JWT 생성

---
+++ src/main/java/com/example/board/util/JWTUtil.java
@@ -0,0 +1,58 @@
+package com.example.board.util;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Log4j2
+public class JWTUtil {
+
+    @Value("${org.zerock.jwt.secret}")
+    private String key;
+
+    public String generateToken(Map<String, Object> valueMap, int days){
+
+        log.info("generateKey..." + key);
+
+        //헤더 부분
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("typ", "JWT");
+        headers.put("alg", "HS256");
+
+        //payload 부분 설정
+        Map<String, Object> payloads = new HashMap<>();
+        payloads.putAll(valueMap);
+
+        // 테스트 시에는 짧은 유효 기간
+        int time = (1) * days; // 테스트는 분단위로 나중에 60*24 (일)단위로 변경
+
+        String jwtStr = Jwts.builder()
+                .setHeader(headers)
+                .setClaims(payloads)
+                .setIssuedAt(Date.from(ZonedDateTime.now().toInstant()))
+                .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(time).toInstant()))
+                .signWith(SignatureAlgorithm.HS256, key.getBytes())
+                .compact();
+
+
+        return jwtStr;
+    }
+
+    public Map<String, Object> validateToken(String Token) throws JwtException {
+
+        Map<String, Object> claim = null;
+
+        return claim;
+    }
+
+
+}
---
+++ src/test/java/com/example/board/util/JWTUtilTests.java
@@ -0,0 +1,26 @@
+package com.example.board.util;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Map;
+
+@SpringBootTest
+@Log4j2
+public class JWTUtilTests {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Test
+    public void testGenerate() {
+
+        Map<String, Object> claimMap = Map.of("mid", "ABCDE");
+
+        String jwtStr = jwtUtil.generateToken(claimMap, 1);
+
+        log.info(jwtStr);
+    }
+}
