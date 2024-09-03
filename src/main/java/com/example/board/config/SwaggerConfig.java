001b87506a435e184186137049501eebdd9981d7
myo22 <dlalsgud12@naver.com>
2024-09-03 18:05:46 +0900

    Swagger UI에서 헤더 처리

--- src/main/java/com/example/board/config/SwaggerConfig.java
+++ src/main/java/com/example/board/config/SwaggerConfig.java
@@ -7,8 +7,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 // @RestController 어노테이션이 있는 컨트롤러들에 대해서 API문서를 생성하도록 작성합니다.
 @Configuration
@@ -23,6 +29,8 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)) // 이 설정은 특정 애너테이션이 붙은 클래스의 메서드만 문서화 대상으로 지정합니다.
                 .paths(PathSelectors.any())
                 .build()
+                .securitySchemes(List.of(apiKey())) // 추가된 부분
+                .securityContexts(List.of(securityContext())) // 추가된 부분
                 .apiInfo(apiInfo());
     }
 
@@ -30,5 +38,18 @@ public class SwaggerConfig {
         return new ApiInfoBuilder()
                 .title("project Swagger")
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Bearer Token", "header");
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .operationSelector(selector -> selector.requestMappingPattern().startsWith("/api/")).build();
+    }
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "global access");
+        return List.of(new SecurityReference("Authorization", new AuthorizationScope[] {authorizationScope}));
     }
 }
