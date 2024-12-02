2e1850858176fc13aac10440863f3f078bc2cd53
myo22 <dlalsgud12@naver.com>
2024-12-02 11:10:19 +0900

    @EnableWebMvc 설정 수정

--- src/main/java/com/example/board/config/CustomServletConfig.java
+++ src/main/java/com/example/board/config/CustomServletConfig.java
@@ -14,13 +14,7 @@ public class CustomServletConfig implements WebMvcConfigurer {
         // '/files/'로 시작하는 경로는 스프링 MVC에서 일반 파일 경로로 처리하도록 지정, 이렇게 안하면 Thymeleaf가 없는 환경에서 스프링 MVC의 모든 경로를 스프링에서 처리하려고 시도하기 때문이다.
         registry.addResourceHandler("/files/**")
                 .addResourceLocations("classpath:/static/");
-//        registry.addResourceHandler("/js/**")
-//                .addResourceLocations("classpath:/static/js/");
-//        registry.addResourceHandler("/fonts/**")
-//                .addResourceLocations("classpath:/static/fonts/");
-//        registry.addResourceHandler("/css/**")
-//                .addResourceLocations("classpath:/static/css/");
-//        registry.addResourceHandler("/assets/**")
-//                .addResourceLocations("classpath:/static/assets/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
